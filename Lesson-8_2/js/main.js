d325bc33889d5699f1b2b297a1f14bf754f109cd
Elena Kuzmich <len.11@mail.ru>
2022-05-15 23:17:27 +0300

    Homework 6

---
+++ Lesson-8_2/js/main.js
@@ -0,0 +1,3 @@
+// #select-1 - селектор для выбора элемента, который необходимо инициализировать как CustomSelect
+const select1 = new CustomSelect('#select-1');
+const select2 = new CustomSelect('#select-2');
